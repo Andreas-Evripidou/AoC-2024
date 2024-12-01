331580ef84583e68bb8baf14fad16a8505bea0f2
Andreas Evripidou <andreasevr@ibm.com>
2024-12-01 13:42:25 +0000

    feat(day 1): done

--- 2024/day01/main.go
+++ 2024/day01/main.go
@@ -2,26 +2,64 @@ package main
 
 import (
 	"fmt"
-	"../utils"
+	"os"
+	"sort"
+	"strconv"
+
+	"github.com/Andreas-Evripidou/AoC-2024/2024/utils"
 )
 
 func main() {
 	input := utils.ReadInput("input.txt")
+	testInput := utils.ReadInput("testInput.txt")
 	fmt.Println("Day 01 input:", input)
 
 	// Part 1
+	fmt.Println("Validate Part 1:", solvePart1(testInput))
 	fmt.Println("Part 1:", solvePart1(input))
 
 	// Part 2
+	fmt.Println("Validate Part 2:", solvePart2(testInput))
 	fmt.Println("Part 2:", solvePart2(input))
 }
 
 func solvePart1(input string) string {
-	// TODO: Implement Part 1
-	return "Not implemented"
+	list1, list2 := utils.ParseTwoNumberLists(input, "   ")
+
+	sort.Ints(list1)
+	sort.Ints(list2)
+
+	sum := 0
+	for index, _ := range list1 {
+		sum += utils.GetIntAbsoluteValue(list1[index] - list2[index])
+	}
+
+	return strconv.Itoa(sum)
 }
 
 func solvePart2(input string) string {
-	// TODO: Implement Part 2
-	return "Not implemented"
+	countMap1, countMap2 := utils.ParsTwoCountMaps(input, "   ")
+
+	sum := 0
+	for key := range countMap1 {
+		num, err := strconv.Atoi(key)
+		if err != nil {
+			fmt.Println("Error:", err)
+			os.Exit(1)
+		}
+
+		sum += num * countMap1[key] * countMap2[key]
+		delete(countMap2, key)
+	}
+	for key := range countMap2 {
+		num, err := strconv.Atoi(key)
+		if err != nil {
+			fmt.Println("Error:", err)
+			os.Exit(1)
+		}
+
+		sum += num * countMap1[key] * countMap2[key]
+	}
+
+	return strconv.Itoa(sum)
 }
--- 2024/utils/utils.go
+++ 2024/utils/utils.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"log"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -13,4 +14,45 @@ func ReadInput(filePath string) string {
 		log.Fatalf("Failed to read file: %v", err)
 	}
 	return strings.TrimSpace(string(data))
+}
+
+func ParsTwoCountMaps(input string, stringBetweenLines string) (map[string]int, map[string]int) {
+	lines := parseLines(input)
+
+	var map1 = make(map[string]int)
+	var map2 = make(map[string]int)
+
+	for _, line := range lines {
+		splitLine := strings.Split(line, stringBetweenLines)
+
+		map1[splitLine[0]]++
+		map2[splitLine[1]]++
+	}
+
+	return map1, map2
+}
+
+func ParseTwoNumberLists(input string, stringBetweenLines string) ([]int, []int) {
+	lines := parseLines(input)
+	fileLength := len(lines)
+
+	list1 := make([]int, fileLength)
+	list2 := make([]int, fileLength)
+	for i, line := range lines {
+		splitLine := strings.Split(line, stringBetweenLines)
+		list1[i], _ = strconv.Atoi(splitLine[0])
+		list2[i], _ = strconv.Atoi(splitLine[1])
+	}
+	return list1, list2
+}
+
+func parseLines(input string) []string {
+	return strings.Split(strings.TrimSpace(input), "\n")
+}
+
+func GetIntAbsoluteValue(num int) int {
+	if num < 0 {
+		return -num
+	}
+	return num
 }
