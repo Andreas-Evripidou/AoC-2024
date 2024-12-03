70b086acee4d4323d11e89c9b1d64ac537d68daf
Andreas Evripidou <Evripidou2001@gmail.com>
2024-12-03 17:48:27 +0000

    feat(day 2): optimize

--- 2024/day02/main.go
+++ 2024/day02/main.go
@@ -27,7 +27,13 @@ func main() {
 func solvePart1(input string) string {
 	reports := utils.ParseListOfListOfInts(input, " ")
 
-	sum := calculateSumOfValidReports(reports, 0)
+	sum := 0
+	for _, report := range reports {
+
+		if isSafeReport(report) {
+			sum++
+		}
+	}
 
 	return strconv.Itoa(sum)
 }
@@ -35,70 +41,54 @@ func solvePart1(input string) string {
 func solvePart2(input string) string {
 	reports := utils.ParseListOfListOfInts(input, " ")
 
-	sum := calculateSumOfValidReports(reports, 1)
+	sum := 0
+	for _, report := range reports {
+
+		if isSafeReport(report) {
+			sum++
+			continue
+		}
+
+		if isSafeReportWithoutOneItem(report) {
+			sum++
+		}
+
+	}
 
 	return strconv.Itoa(sum)
 }
 
-func calculateSumOfValidReports(reports [][]int, toleration int) int {
-	sum := 0
-	for _, levels := range reports {
-
-		if !isSortedWithTolerance(levels, toleration) {
-			continue
+func isSafeReportWithoutOneItem(report []int) bool {
+	for itemToRemove := 0; itemToRemove < len(report); itemToRemove++ {
+		newReport := append([]int{}, report[0:itemToRemove]...)
+		newReport = append(newReport, report[itemToRemove+1:]...)
+		if isSafeReport(newReport) {
+			return true
 		}
-
-		if areValidLevels(levels) {
-			sum++
-			continue
-		}
-		if toleration == 0 {
-			continue
-		}
-
-		for itemToRemove := 0; itemToRemove < len(levels); itemToRemove++ {
-			newLevels := append([]int{}, levels[:itemToRemove]...)    // Elements before the i-th
-			newLevels = append(newLevels, levels[itemToRemove+1:]...) // Elements after the i-th
-			if areValidLevels(newLevels) {
-				sum++
-				break
-			}
-		}
-
 	}
-	return sum
+	return false
 }
 
-func areValidLevels(levels []int) bool {
-	for index := 0; index < len(levels)-1; index++ {
-		distance := utils.GetIntAbsoluteValue(levels[index] - levels[index+1])
+func isSafeReport(report []int) bool {
+	ascViolations, descViolations := 0, 0
+
+	for index := 0; index < len(report)-1; index++ {
+		distance := utils.GetIntAbsoluteValue(report[index] - report[index+1])
 
 		if distance < 1 || distance > 3 {
 			return false
 		}
 
-	}
-	return true
-}
-
-func isSortedWithTolerance(slice []int, toleration int) bool {
-	if len(slice) < 2 {
-		return true
-	}
-
-	ascViolations, descViolations := 0, 0
-
-	for i := 1; i < len(slice); i++ {
-		if slice[i] < slice[i-1] {
+		if report[index] > report[index+1] {
 			ascViolations++
-		} else if slice[i] > slice[i-1] {
+		} else if report[index] < report[index+1] {
 			descViolations++
 		}
 
-		if ascViolations > toleration && descViolations > toleration {
+		if ascViolations > 0 && descViolations > 0 {
 			return false
 		}
-	}
 
+	}
 	return true
 }
